5f3b0de24e92d7805d3faf333f83f1df5072df06
Tran Le Tan Tai <1824801030018@student.tdmu.edu.vn>
2021-09-10 15:17:23 +0700

    update ScanHistory by JD

--- src/screens/ScanHistory/index.js
+++ src/screens/ScanHistory/index.js
@@ -16,74 +16,11 @@ import data from './data'
 import {COLORS} from '../../constants/theme'
 
 
-// const Category = () => {
-//   return (
-//     <View>
-//       <Header back title="Lịch sử quét mã" />
-//       <View style={styles.container}>
-//         <TouchableOpacity style={styles.itemHistory}>
-//           <AntDesign
-//             style={styles.iconQR}
-//             name="qrcode"
-//             size={36}
-//             color="white"
-//           />
-//           <View>
-//             <Text numberOfLines={1} style={styles.textName}>
-//               Điền gì chưa biết nên cứ để đây lúc nào biết thì điền
-//             </Text>
-//             <Text style={styles.dateTime}>10:09 - 10/09/2021</Text>
-//           </View>
-//         </TouchableOpacity>
-//         <TouchableOpacity style={styles.itemHistory}>
-//           <AntDesign
-//             style={styles.iconQR}
-//             name="qrcode"
-//             size={36}
-//             color="white"
-//           />
-//           <View>
-//             <Text numberOfLines={1} style={styles.textName}>
-//               Điền gì chưa biết nên cứ để đây lúc nào biết thì điền
-//             </Text>
-//             <Text style={styles.dateTime}>10:09 - 10/09/2021</Text>
-//           </View>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   );
-// };
-
-
 const Category = () => {
   return (
     <>
       <Header back title="Lịch sử quét mã" />
       <View style={styles.container}>
-        {/* <Timeline
-          style={styles.list}
-          data={data}
-          circleSize={20}
-          circleColor={COLORS.primary}
-          lineColor="black"
-          timeContainerStyle={{minWidth: 52}}
-          listViewContainerStyle={{marginTop: 5}}
-          timeStyle={{
-            textAlign: 'center',
-            backgroundColor: COLORS.primary,
-            color: 'white',
-            padding: 5,
-            borderRadius: 13,
-          }}
-          titleStyle={{marginTop: -12}}
-          descriptionStyle={{color: 'gray'}}
-          options={{
-            style: {paddingTop: 10, paddingRight: 20, paddingLeft: 20},
-            removeClippedSubviews: false,
-          }}
-          innerCircle={'dot'}
-          columnFormat='two-column'
-        /> */}
         <Timeline
           style={styles.list}
           data={data}
--- src/screens/ScanHistory/style.js
+++ src/screens/ScanHistory/style.js
@@ -7,6 +7,14 @@ export const styles = StyleSheet.create({
   },
   list: {
     flex: 1,
-    marginRight: 10
+    marginRight: 10,
+  },
+  timeStyles: {
+    textAlign: 'center',
+    backgroundColor: COLORS.primary,
+    color: 'white',
+    padding: 5,
+    borderBottomRightRadius: 30,
+    borderTopRightRadius: 30,
   },
 });
